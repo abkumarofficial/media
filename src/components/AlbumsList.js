f705d58d35d60bfb7ffb951d3165cc5bb69d49d3
Abhijeet Kumar <abkumarofficial@gmail.com>
2024-02-21 20:57:08 +0530

    RTQ First Commit

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,13 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
+
 function AlbumsList({ user }) {
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+    console.log(data, error, isLoading);
+
     return <div>Albums for {user.name}</div>
 }
 
